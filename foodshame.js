f946e539f15f82a5f8a539ad66934d6881dd6851
thisisbrianhan <bthan@us.ibm.com>
2015-01-31 23:49:47 -0600

    I HAZ PHOTOS FROM ZE WEBCAMZ

--- foodshame.js
+++ foodshame.js
@@ -2,6 +2,20 @@ if (Meteor.isClient) {
   Template.takePhoto.events({
     'click .capture': function () {
       console.log("button clicked.");
+      MeteorCamera.getPicture({}, function (error, data) {
+        if (error) {
+          console.log("error: ", error);
+        }
+
+        Session.set('photo', data);
+        // console.log("data: ", data);
+      });
     }
   });
+
+  Template.takePhoto.helpers({
+    'photo': function() {
+      return Session.get('photo');
+    }
+  })
 }
